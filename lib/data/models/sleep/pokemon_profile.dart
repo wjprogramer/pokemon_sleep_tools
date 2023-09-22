98e04a2831c30b78378f4bdd39421d46108e23f8
jay_wu <weeihua1003@gmail.com>
2023-09-22 12:00:31 +0800

    _

--- lib/data/models/sleep/pokemon_profile.dart
+++ lib/data/models/sleep/pokemon_profile.dart
@@ -51,31 +51,31 @@ class PokemonProfile {
 
     var result = '${basicProfile.nameI18nKey}\n'
         '性格: ${character.name}\n'
-        '副技能:\n'
-        '    Lv 10: ${subSkillLv10.name}\n'
-        '    Lv 25: ${subSkillLv25.name}\n'
-        '    Lv 50: ${subSkillLv50.name}\n'
-        '    Lv 75: ${subSkillLv75.name}\n'
-        '    Lv 100: ${subSkillLv100.name} \n'
-        '食材:\n'
-        '    ${ingredient1.nameI18nKey} (${ingredientCount1})\n'
-        '    ${ingredient2.nameI18nKey} (${ingredientCount2})\n'
-        '    ${ingredient3.nameI18nKey} (${ingredientCount3})\n'
+        // '副技能:\n'
+        // '    Lv 10: ${subSkillLv10.name}\n'
+        // '    Lv 25: ${subSkillLv25.name}\n'
+        // '    Lv 50: ${subSkillLv50.name}\n'
+        // '    Lv 75: ${subSkillLv75.name}\n'
+        // '    Lv 100: ${subSkillLv100.name} \n'
+        // '食材:\n'
+        // '    ${ingredient1.nameI18nKey} (${ingredientCount1})\n'
+        // '    ${ingredient2.nameI18nKey} (${ingredientCount2})\n'
+        // '    ${ingredient3.nameI18nKey} (${ingredientCount3})\n'
         '幫忙均能: TBD\n'
         '類型: ${basicProfile.sleepType.name} (${basicProfile.fruit.nameI18nKey}) \n'
         '數量: ${fruitCount}\n'
         '幫忙間隔: TBD\n'
         '幫忙均能/次: TBD\n'
-        '類型: TBD\n'
-        '樹果: TBD\n'
-        '數量: TBD\n'
+        // '類型: ${basicProfile.sleepType.name}\n'
+        // '樹果: ${basicProfile.fruit.nameI18nKey}\n'
+        '數量: ${fruitCount}\n'
         '幫忙間隔: TBD\n'
-        '樹果能量: $fruitEnergy\n'
-        '食材1能量: $ingredientEnergy1\n'
-        '食材2能量: $ingredientEnergy2\n'
-        '食材3能量: $ingredientEnergy3\n'
-        '食材均能: $ingredientEnergyAvg\n'
-        '幫手獎勵: TBD\n'
+        // '樹果能量: $fruitEnergy\n'
+        // '食材1能量: $ingredientEnergy1\n'
+        // '食材2能量: $ingredientEnergy2\n'
+        // '食材3能量: $ingredientEnergy3\n'
+        // '食材均能: $ingredientEnergyAvg\n'
+        '幫手獎勵: ${calcHelperBonus()}\n'
         '幫忙速度S: TBD\n'
         '幫忙速度M: TBD\n'
         '食材機率: TBD\n'
@@ -94,6 +94,18 @@ class PokemonProfile {
 
 
     return result;
+  }
+
+  int calcHelperBonus() {
+    var res = 0;
+
+    for (final subSkill in subSkills) {
+      if (subSkill == SubSkill.s2) {
+        res += 15;
+      }
+    }
+
+    return res;
   }
 
   int calcFruitCount() {
