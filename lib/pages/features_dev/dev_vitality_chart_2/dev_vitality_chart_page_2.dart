ab4e884aeaec84a0ebe6b4a034449a0a922fed1e
WJ Programmer <weeihua1003@gmail.com>
2023-10-29 15:05:09 +0800

    vitality_chart: clean

--- lib/pages/features_dev/dev_vitality_chart_2/dev_vitality_chart_page_2.dart
+++ lib/pages/features_dev/dev_vitality_chart_2/dev_vitality_chart_page_2.dart
@@ -13,6 +13,8 @@ import 'package:pokemon_sleep_tools/widgets/sleep/charts/vitality_chart/vitality
 import 'package:pokemon_sleep_tools/widgets/sleep/images/images.dart';
 import 'package:reactive_forms/reactive_forms.dart';
 
+part 'src/vitality_utils.dart';
+
 /// 每一個 table spot step 代表 5 分鐘
 const _spotStepValue = 5;
 
@@ -76,26 +78,26 @@ class _DevVitalityChartPage2State extends State<DevVitalityChartPage2> {
       value: _fixOffsetTimeOfDay(const TimeOfDay(hour: 21, minute: 0)),
       validators: [ Validators.required ],
     )..valueChanges.listen((event) {
-      _prepareChartData();
+      prepareData();
       setState(() { });
     });
     _mainGetUpTimeField = FormControl(
       value: _fixOffsetTimeOfDay(const TimeOfDay(hour: 5, minute: 0)),
       validators: [ Validators.required ],
     )..valueChanges.listen((event) {
-      _prepareChartData();
+      prepareData();
       setState(() { });
     });
     _extraSleepTimeField = FormControl(
       validators: [],
     )..valueChanges.listen((event) {
-      _prepareChartData();
+      prepareData();
       setState(() { });
     });
     _extraGetUpTimeField = FormControl(
       validators: [],
     )..valueChanges.listen((event) {
-      _prepareChartData();
+      prepareData();
       setState(() { });
     });
     // _sleepScoreField = FormControl();
@@ -109,12 +111,28 @@ class _DevVitalityChartPage2State extends State<DevVitalityChartPage2> {
         _initVitalityDebounce?.cancel();
       }
       _initVitalityDebounce = Timer(const Duration(milliseconds: 200), () {
-        _prepareChartData();
+        prepareData();
         setState(() { });
       });
     });
 
-    _prepareChartData();
+    prepareData();
+  }
+
+  void prepareData() {
+    final res = _VitalityUtils.prepareData(
+      mainSleepTime: _mainSleepTimeField.value!,
+      mainGetUpTime: _mainGetUpTimeField.value!,
+      initVitality: _initVitalityField.value?.clamp(0.0, MAX_VITALITY).toDouble(),
+      isInitVitalityWhenGetUp: _isInitVitalityWhenGetUp,
+    );
+
+    _showingTooltipSpotIndexList = res.showingTooltipSpotIndexList;
+    _tableDataList = res.tableDataList;
+    _stops = res.stops;
+    _colors = res.colors;
+    _sleepElapsed = res.sleepElapsed;
+    _mainSleepScore = res.mainSleepScore;
   }
 
   Future<TimeOfDay?> _pickTime({
@@ -135,29 +153,6 @@ class _DevVitalityChartPage2State extends State<DevVitalityChartPage2> {
       hour: time.hour,
       minute: time.minute - time.minute % 5,
     );
-  }
-
-  TimeOfDay _calcTimeElapsed(TimeOfDay sleepTime, TimeOfDay getUpTime) {
-    final sleepMinutes = sleepTime.toMinutes();
-    final getUpMinutes = getUpTime.toMinutes();
-    const minutesOfDay = 24 * 60;
-
-    int sleepElapsedMinutes;
-    if (sleepMinutes == getUpMinutes) {
-      sleepElapsedMinutes = 0;
-    } else if (sleepMinutes > getUpMinutes) {
-      // 僅列小時: 22 ~ 6, 10,6
-      sleepElapsedMinutes = minutesOfDay - sleepMinutes + getUpMinutes;
-    } else {
-      // 僅列小時: 6 ~ 10
-      sleepElapsedMinutes = getUpMinutes - sleepMinutes;
-    }
-
-    if (sleepElapsedMinutes < 0) {
-      sleepElapsedMinutes += minutesOfDay;
-    }
-
-    return TimeOfDay(hour: sleepElapsedMinutes ~/ 60, minute: sleepElapsedMinutes % 60);
   }
 
   // TimeOfDay _toTimeFromMinutes(int minutes) {
@@ -233,132 +228,12 @@ class _DevVitalityChartPage2State extends State<DevVitalityChartPage2> {
     );
   }
 
-  void _prepareChartData() {
-    var foundZero = false;
-    var sleeping = false;
-    final double? initVitality = _initVitalityField.value?.clamp(0.0, MAX_VITALITY).toDouble();
-    double lastVitalitySpotValue;
-    final sleepTime = _mainSleepTimeField.value!;
-    final getUpTime = _mainGetUpTimeField.value!;
-    var lastTimeSpot = const TimeOfDay(hour: 0, minute: 0);
-    var lastTimeSpotIndex = 0;
-    TimeOfDay tableInitTime;
-    var preVitality = 0.0;
-
-    if (_isInitVitalityWhenGetUp) {
-      sleeping = false;
-      lastTimeSpot = getUpTime;
-      lastVitalitySpotValue = initVitality ?? 100.0;
-      tableInitTime = getUpTime;
-    } else {
-      sleeping = true;
-      lastTimeSpot = sleepTime;
-      lastVitalitySpotValue = initVitality ?? 100.0;
-      tableInitTime = sleepTime;
-    }
-
-    final elapsedTime = _calcTimeElapsed(sleepTime, getUpTime);
-    final mainSleepScore = ((elapsedTime.hour * 60.0 + elapsedTime.minute) / 510 * 100).clamp(0.0, 100.0);
-
-    // final tableAxisDuration = _calcTableDuration(sleepTime, getUpTime);
-    const tableAxisDuration = TimeOfDay(hour: 24, minute: 0);
-    final tableCounts = (tableAxisDuration.hour * 60 + tableAxisDuration.minute) ~/ 5 + 1;
-    final showingTooltipSpotIndexList = <int>[];
-
-    final stops = <double>[];
-    final colors = <Color>[];
-
-    int? tmpVitalityThreshold;
-
-    VitalityChartData getSpotValue(TimeOfDay initTime, int index) {
-      final time = initTime.add(minute: index * _spotStepValue);
-
-      // 開始睡覺
-      final isSleepTooltipSpot = time == sleepTime;
-      if (isSleepTooltipSpot) {
-        sleeping = true;
-        lastTimeSpot = time;
-        lastTimeSpotIndex = index;
-        if (index != 0) {
-          lastVitalitySpotValue = (
-              lastVitalitySpotValue - ((index - 1) * _spotStepValue) / 10
-          ).clamp(0.0, 100.0);
-        }
-      }
-
-      // 起床
-      final isGetUpTooltipSpot = time == getUpTime;
-      if (isGetUpTooltipSpot) {
-        lastVitalitySpotValue = (
-            lastVitalitySpotValue + (_calcTimeElapsed(lastTimeSpot, time).toMinutes() / 510 * 100)
-        ).clamp(0, 100);
-        lastTimeSpot = time;
-        lastTimeSpotIndex = index;
-        sleeping = false;
-      }
-
-      final vitality = sleeping ? (
-        lastVitalitySpotValue + (_calcTimeElapsed(lastTimeSpot, time).toMinutes() / 510 * 100)
-      ).clamp(0.0, 100.0) : (
-          lastVitalitySpotValue - ((index - lastTimeSpotIndex) * _spotStepValue) / 10
-      ).clamp(0.0, 100.0);
-      final (vitalityThreshold, vitalityColor) = isSleepTooltipSpot || sleeping
-          ? (100, moodColor80)
-          : MoodIcon.getVitalityThresholdAndColor(vitality);
-      final showBottomTitle = index % 30 == 0;
-
-
-      var showTooltip = (vitality != 0 && vitality % 20.0 == 0 && preVitality != vitality) ||
-          (vitality == 0 && !foundZero);
-      preVitality = vitality;
-      if (vitality == 0) {
-        foundZero = true;
-      }
-      showTooltip = showTooltip || index == 0;
-      if (tmpVitalityThreshold != vitalityThreshold) {
-        final stopValue = (index - 1).clamp(0, double.infinity) / (tableCounts - 1);
-        if (tmpVitalityThreshold != null) {
-          colors.add(MoodIcon.getColorBy(tmpVitalityThreshold!));
-          stops.add(stopValue);
-        }
-        colors.add(vitalityColor);
-        stops.add(stopValue);
-      }
-      tmpVitalityThreshold = vitalityThreshold;
-      final timeText = MyFormatter.time(time);
-
-      showTooltip = showTooltip || isGetUpTooltipSpot || isSleepTooltipSpot;
-      if (showTooltip) {
-        showingTooltipSpotIndexList.add(index);
-      }
-
-      return VitalityChartData(
-          spot: FlSpot(index.toDouble(), vitality),
-          time: timeText,
-          isShowBottomTitle: showBottomTitle,
-          isShowTooltip: showTooltip,
-          tooltipText: isGetUpTooltipSpot ? '$timeText 起床\n${vitality.toInt()}'
-              : isSleepTooltipSpot ? '$timeText 睡覺\n${vitality.toInt()}'
-              : null
-      );
-    }
-
-    final tableDataList = List.generate(tableCounts, (index) => getSpotValue(tableInitTime, index));
-
-    _showingTooltipSpotIndexList = showingTooltipSpotIndexList;
-    _tableDataList = tableDataList;
-    _stops = stops;
-    _colors = colors;
-    _sleepElapsed = elapsedTime;
-    _mainSleepScore = mainSleepScore;
-  }
-
   void _test() {
     testTime(int sleepHour, int sleepMinute, int getUpHour, int getUpMinute) {
       final sleepTime = TimeOfDay(hour: sleepHour, minute: sleepMinute);
       final getUpTime = TimeOfDay(hour: getUpHour, minute: getUpMinute);
 
-      final elapsed = _calcTimeElapsed(sleepTime, getUpTime);
+      final elapsed = _VitalityUtils.calcTimeElapsed(sleepTime, getUpTime);
       const display = MyFormatter.time;
       debugPrint('${display(sleepTime)} 睡覺 , 經過 ${display(elapsed)} 後，起床 ${display(getUpTime)}');
     }
@@ -376,7 +251,7 @@ class _DevVitalityChartPage2State extends State<DevVitalityChartPage2> {
       final sleepTime = data.sleepTime;
       final getUpTime = data.getUpTime;
       const minutesOfDay = 24 * 60;
-      final elapsed = _calcTimeElapsed(sleepTime, getUpTime);
+      final elapsed = _VitalityUtils.calcTimeElapsed(sleepTime, getUpTime);
       final elapsedMinutes = elapsed.toMinutes();
 
       var result = false;
@@ -536,7 +411,7 @@ class _DevVitalityChartPage2State extends State<DevVitalityChartPage2> {
                                 return;
                               }
                               _isInitVitalityWhenGetUp = true;
-                              _prepareChartData();
+                              prepareData();
                               setState(() { });
                             },
                             child: Row(
@@ -561,7 +436,7 @@ class _DevVitalityChartPage2State extends State<DevVitalityChartPage2> {
                                 return;
                               }
                               _isInitVitalityWhenGetUp = false;
-                              _prepareChartData();
+                              prepareData();
                               setState(() { });
                             },
                             child: Row(
---
+++ lib/pages/features_dev/dev_vitality_chart_2/src/vitality_utils.dart
@@ -0,0 +1,174 @@
+part of '../dev_vitality_chart_page_2.dart';
+
+class _VitalityUtils {
+  _VitalityUtils._();
+
+  static _VitalityChartResult prepareData({
+    required TimeOfDay mainSleepTime,
+    required TimeOfDay mainGetUpTime,
+    required double? initVitality,
+    required bool isInitVitalityWhenGetUp,
+  }) {
+    var foundZero = false;
+    var sleeping = false;
+    double lastVitalitySpotValue;
+    final sleepTime = mainSleepTime;
+    final getUpTime = mainGetUpTime;
+    var lastTimeSpot = const TimeOfDay(hour: 0, minute: 0);
+    var lastTimeSpotIndex = 0;
+    TimeOfDay tableInitTime;
+    var preVitality = 0.0;
+
+    if (isInitVitalityWhenGetUp) {
+      sleeping = false;
+      lastTimeSpot = getUpTime;
+      lastVitalitySpotValue = initVitality ?? 100.0;
+      tableInitTime = getUpTime;
+    } else {
+      sleeping = true;
+      lastTimeSpot = sleepTime;
+      lastVitalitySpotValue = initVitality ?? 100.0;
+      tableInitTime = sleepTime;
+    }
+
+    final elapsedTime = calcTimeElapsed(sleepTime, getUpTime);
+    final mainSleepScore = ((elapsedTime.hour * 60.0 + elapsedTime.minute) / 510 * 100).clamp(0.0, 100.0);
+
+    // final tableAxisDuration = _calcTableDuration(sleepTime, getUpTime);
+    const tableAxisDuration = TimeOfDay(hour: 24, minute: 0);
+    final tableCounts = (tableAxisDuration.hour * 60 + tableAxisDuration.minute) ~/ 5 + 1;
+    final showingTooltipSpotIndexList = <int>[];
+
+    final stops = <double>[];
+    final colors = <Color>[];
+
+    int? tmpVitalityThreshold;
+
+    VitalityChartData getSpotValue(TimeOfDay initTime, int index) {
+      final time = initTime.add(minute: index * _spotStepValue);
+
+      // 開始睡覺
+      final isSleepTooltipSpot = time == sleepTime;
+      if (isSleepTooltipSpot) {
+        sleeping = true;
+        lastTimeSpot = time;
+        lastTimeSpotIndex = index;
+        if (index != 0) {
+          lastVitalitySpotValue = (
+              lastVitalitySpotValue - ((index - 1) * _spotStepValue) / 10
+          ).clamp(0.0, 100.0);
+        }
+      }
+
+      // 起床
+      final isGetUpTooltipSpot = time == getUpTime;
+      if (isGetUpTooltipSpot) {
+        lastVitalitySpotValue = (
+            lastVitalitySpotValue + (calcTimeElapsed(lastTimeSpot, time).toMinutes() / 510 * 100)
+        ).clamp(0, 100);
+        lastTimeSpot = time;
+        lastTimeSpotIndex = index;
+        sleeping = false;
+      }
+
+      final vitality = sleeping ? (
+          lastVitalitySpotValue + (calcTimeElapsed(lastTimeSpot, time).toMinutes() / 510 * 100)
+      ).clamp(0.0, 100.0) : (
+          lastVitalitySpotValue - ((index - lastTimeSpotIndex) * _spotStepValue) / 10
+      ).clamp(0.0, 100.0);
+      final (vitalityThreshold, vitalityColor) = isSleepTooltipSpot || sleeping
+          ? (100, moodColor80)
+          : MoodIcon.getVitalityThresholdAndColor(vitality);
+      final showBottomTitle = index % 30 == 0;
+
+
+      var showTooltip = (vitality != 0 && vitality % 20.0 == 0 && preVitality != vitality) ||
+          (vitality == 0 && !foundZero);
+      preVitality = vitality;
+      if (vitality == 0) {
+        foundZero = true;
+      }
+      showTooltip = showTooltip || index == 0;
+      if (tmpVitalityThreshold != vitalityThreshold) {
+        final stopValue = (index - 1).clamp(0, double.infinity) / (tableCounts - 1);
+        if (tmpVitalityThreshold != null) {
+          colors.add(MoodIcon.getColorBy(tmpVitalityThreshold!));
+          stops.add(stopValue);
+        }
+        colors.add(vitalityColor);
+        stops.add(stopValue);
+      }
+      tmpVitalityThreshold = vitalityThreshold;
+      final timeText = MyFormatter.time(time);
+
+      showTooltip = showTooltip || isGetUpTooltipSpot || isSleepTooltipSpot;
+      if (showTooltip) {
+        showingTooltipSpotIndexList.add(index);
+      }
+
+      return VitalityChartData(
+          spot: FlSpot(index.toDouble(), vitality),
+          time: timeText,
+          isShowBottomTitle: showBottomTitle,
+          isShowTooltip: showTooltip,
+          tooltipText: isGetUpTooltipSpot ? '$timeText 起床\n${vitality.toInt()}'
+              : isSleepTooltipSpot ? '$timeText 睡覺\n${vitality.toInt()}'
+              : null
+      );
+    }
+
+    final tableDataList = List.generate(tableCounts, (index) => getSpotValue(tableInitTime, index));
+
+    return _VitalityChartResult(
+      showingTooltipSpotIndexList: showingTooltipSpotIndexList,
+      tableDataList: tableDataList,
+      stops: stops,
+      colors: colors,
+      sleepElapsed: elapsedTime,
+      mainSleepScore: mainSleepScore,
+    );
+  }
+
+  static TimeOfDay calcTimeElapsed(TimeOfDay sleepTime, TimeOfDay getUpTime) {
+    final sleepMinutes = sleepTime.toMinutes();
+    final getUpMinutes = getUpTime.toMinutes();
+    const minutesOfDay = 24 * 60;
+
+    int sleepElapsedMinutes;
+    if (sleepMinutes == getUpMinutes) {
+      sleepElapsedMinutes = 0;
+    } else if (sleepMinutes > getUpMinutes) {
+      // 僅列小時: 22 ~ 6, 10,6
+      sleepElapsedMinutes = minutesOfDay - sleepMinutes + getUpMinutes;
+    } else {
+      // 僅列小時: 6 ~ 10
+      sleepElapsedMinutes = getUpMinutes - sleepMinutes;
+    }
+
+    if (sleepElapsedMinutes < 0) {
+      sleepElapsedMinutes += minutesOfDay;
+    }
+
+    return TimeOfDay(hour: sleepElapsedMinutes ~/ 60, minute: sleepElapsedMinutes % 60);
+  }
+
+}
+
+class _VitalityChartResult {
+  _VitalityChartResult({
+    required this.showingTooltipSpotIndexList,
+    required this.tableDataList,
+    required this.stops,
+    required this.colors,
+    required this.sleepElapsed,
+    required this.mainSleepScore,
+  });
+
+  List<int> showingTooltipSpotIndexList;
+  List<VitalityChartData> tableDataList;
+  List<double> stops;
+  List<Color> colors;
+  TimeOfDay sleepElapsed;
+  double mainSleepScore;
+
+}
